004e2ef64dee4a1cebc85928c0e4fdf8625e2f06
Dr. Narayan Jethwani <narayan.jethwani@gmail.com>
2026-08-03 11:28:27 +0530

    feat: finalize care pathway pricing and branded invoices

---
+++ src/lib/clinicBranding.ts
@@ -0,0 +1,3 @@
+export const CLINIC_BRAND_NAME = "Homeo Healthcare";
+export const CLINIC_LOGO_PATH = "/images/logo.png";
+export const CLINIC_LOGO_PUBLIC_URL = "https://www.homeo.healthcare/images/logo.png";
